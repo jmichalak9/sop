05d5d7d77f3d8ffe344a760e00548d8154842c6d
Damian Opoka <damian.opoka99@gmail.com>
2019-11-10 12:52:54 +0100

    Zmiana nazwy i drobne poprawki w kodzie.

--- lab1.c
+++ lab1.c
@@ -1,162 +1,168 @@
-
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <math.h>
+#include <string.h>
+#include <ftw.h>
+#include <unistd.h>
+#include <time.h>
+#include <dirent.h>
+
+#define MAX_ELEMS 1000
+#define BUFF 20
+#define SUM 100
+#define MAX_LINE 20
 #define ERR(source) (perror(source),\
-            fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
-            exit(EXIT_FAILURE))
-#define MAX_NUMS 10
-#define BUF_SIZE 500
+		     fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
+		     exit(EXIT_FAILURE))
 
-extern char *optarg;
-extern int optind;
-// globalne, zeby dalo sie odczytac przy funkcji makedir z atexit
-char dir[BUF_SIZE];
-int a[MAX_NUMS], nums;
-
-void usage(char* pname) {
-    fprintf(stderr, "Usage: %s [OPTION]...\n"
-                    "Sprawdza, czy ciag jest ciagiem geometrycznym.\n"
-                    "  -l LICZBA\t\tkolejny wyraz ciagu, min. 2, max. 10\n"
+void usage(char* pname){
+	fprintf(stderr,"Usage: %s [OPTION]...\n"
+                    "Sprawdza, czy ciag jest ciagiem arytmetycznym.\n"
+                    "  -l LICZBA\t\tkolejny wyraz ciagu\n"
                     "  -k KATALOG\t\ttworzy KATALOG spelniajacy warunki zadania\n"
                     "  -p \t\ttworzy plik, tylko z flaga -k\n",
                     pname);
-    exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
-int cmp(const void * a, const void * b)
+extern int optind;
+extern char* optarg;
+
+
+int my_cmp (const void * a, const void * b)
 {
     int _a = *(int*)a;
     int _b = *(int*)b;
-    if(_a < _b) return -1;
+    if(_a < _b) return 1;
     else if(_a == _b) return 0;
-    else return 1;
+    else return -1;
 }
 
-void niegeom() {
-    printf("Nie jest geometryczny.\n");
-    exit(EXIT_SUCCESS);
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    if(remove(fpath))ERR("unlink");
+    return 0;
 }
 
-float sum(int a, float q, int n) {
-    float s = a * (1-pow(q, n)) / (1-q);
-    return s;
-}
-
-void makedir() {
-    struct stat st;
-    if(stat(dir,&st) == 0) {
-        printf("%s is present, removing...\n", dir);
-        if (rmdir(dir))
-            ERR("rmdir");
+void makedir(char* name,int* tab,int ind)
+{
+    DIR *dirp;
+    if(NULL !=(dirp = opendir(name)))
+    {
+        if(nftw(name, unlink_cb, 64, FTW_DEPTH | FTW_PHYS)!=0) ERR("nftw");
+        if(closedir(dirp)) ERR("closedir");
     }
-    if (mkdir(dir, 0700))
-        ERR("mkdir");
-    if (chdir(dir))
-        ERR("chdir");
-    srand(time(NULL));
-    for (int i = 0; i < nums; i++) {
-        int x = rand() % 4;
-        char buf[BUF_SIZE];
-        snprintf(buf, BUF_SIZE, "%d", a[i]);
-        if (x == 0) {
-            FILE* f;
-            if((f=fopen(buf,"w+")) == NULL)
-                ERR("fopen");
-            if (fclose(f))
-                ERR("fclose");
-        } else {
-            if (symlink(".", buf))
-                ERR("symlink");
+
+    if(mkdir(name,0700)) ERR("mkdir");
+    if(chdir(name))ERR("chdir");
+
+    FILE* s1;
+    for(int i=0;i<ind;i++)
+    {
+        int rnd = (rand()%5) + 1;
+        char tmp[MAX_LINE];
+        if(sprintf(tmp, "%d", tab[i])<0) ERR("sprintf");
+        if(rnd <= 2)
+        {
+            if((s1=fopen(tmp,"w+"))==NULL)ERR("fopen");
+            if(fclose(s1))ERR("fclose");
         }
-       
+        else
+        {
+            if(symlink(".",tmp)!=0) ERR("symlink");
+        }
     }
+    if(chdir(".."))ERR("chdir");
+    
 }
 
-
-void makefile() {
-    FILE* f;
-    if((f=fopen("plik","w+")) == NULL)
-            ERR("fopen");
-    for (int i = 0; i < nums; i++) {
-        fprintf(f, "%d ", a[i]); 
-       
-    }
-    if (fclose(f))
-        ERR("fclose");
-
+void mfile(char *name,int* tab,int ind){
+        FILE* s1;
+        strcat(name,"f");
+        if((s1=fopen(name,"w+"))==NULL)ERR("fopen");
+        for(int i=0;i<ind;i++)
+        {
+            fprintf(s1,"%d ",tab[i]);
+        }
+        if(fclose(s1))ERR("fclose");
+        name[strlen(name)-1] = '\0';
 }
 
-int main(int argc, char** argv) {
-	int c = 0, elemsToSum = 100, dirSet = 0;
-	while ((c = getopt (argc, argv, "l:k:p")) != -1) {
-		switch (c)
-		{
+int main(int argc, char** argv)
+{
+    int ind=0;
+    int tab[MAX_ELEMS];
+    int c;
+    int x;
+    int num = SUM;
+    int optp = 0;
+    char name[BUFF] = "";
+	while ((c = getopt (argc, argv, "l:k:p")) != -1)
+		switch (c){
 			case 'l':
-                if (nums >= MAX_NUMS)
-                    usage(argv[0]);
-				a[nums]=atoi(optarg);
-                nums++;
+				x=strtol(optarg, (char **)NULL, 8);
+                if (x==-1) usage(argv[0]);
+                tab[ind++]=x;
 				break;
-			case 'k':
-                dirSet = 1;
-                strncpy(dir, optarg, BUF_SIZE);
-                atexit(makedir);
+            case 'k':
+                strcpy(name,optarg);
                 break;
             case 'p':
-                if (dirSet == 0)
-                    usage(argv[0]);
-                atexit(makefile);
+                optp=1;
+                break;
 			case '?':
-			default:
-				usage(argv[0]);
+			default: usage(argv[0]);
 		}
-    }
-    if(argc>optind) usage(argv[0]);
+	if(argc>optind) usage(argv[0]);
 
-    if (nums < 2)
+    qsort(tab,ind,sizeof(int),my_cmp);
+
+    int r = tab[1]-tab[0];
+    for(int i=2;i<ind;i++)
+    {
+        if(tab[i]-tab[i-1]!=r)
+            return EXIT_FAILURE;
+    }
+    printf("Ciag jest arytmetyczny!\n");
+
+    if(strcmp(name,"")!=0)
+    {
+        srand(time(NULL));
+        makedir(name,tab,ind);
+    }
+
+    if(optp==1 && strcmp(name,"")!=0)
+    {
+        mfile(name,tab,ind);
+    }
+    else
+    {
         usage(argv[0]);
-    qsort(a, nums, sizeof(int), cmp);
-
-    // sprawdzanie czy geometryczny
-    if (a[0] == 0)
-        niegeom();
-
-    float ratio = a[1] / a[0];
-    for (int i = 1; i < nums; i++) {
-        if (a[i-1] == 0)
-            niegeom();
-        if ((float)a[i]/a[i-1] != ratio)
-            niegeom();
     }
 
-    printf("Jest geometryczny.\n");
-
-    // DEBUG
-    char ch;
-    while(scanf("%c", &ch) == 1) {
-        char buf[BUF_SIZE];
-        switch(ch) {
-        case 'z':
-            snprintf(buf, BUF_SIZE, "SUM=%f", sum(a[0], ratio, elemsToSum) );
-            if(putenv(buf)!=0) {
-		        ERR("putenv");
-	        }
-            break;
-        case 'k':
-            
-            printf("%s\n", getenv("SUM"));
-            exit(EXIT_SUCCESS);
-            break;
-        case 'l':
-            scanf("%d", &elemsToSum);
-            break;
+    char a;
+    if(putenv("SUM=NieZapisanoSumy")!=0) ERR("putenv");
+    while(scanf("%c",&a) == 1)
+    {
+        int sum=0;
+        char buff[MAX_LINE];
+        switch(a)
+        {
+            case 's':
+                if(ind<num) usage(argv[0]);
+                sum = (tab[0]+tab[num-1])*(num)/2;
+                snprintf(buff,MAX_LINE,"SUM=%d",sum);
+                if(putenv(buff)!=0) ERR("putenv");
+                break;
+            case 'x':
+                printf("%s\n",getenv("SUM"));
+                break;
+            case 'n':
+                scanf("%d",&num);
+                break;
         }
     }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
